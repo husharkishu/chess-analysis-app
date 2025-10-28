18b552047d86f130ac14521bf2ad63786bd30ed8
husharkishu <138245235+husharkishu@users.noreply.github.com>
2025-10-28 16:46:50 +0530

    Create MainActivity.kt

---
+++ app/src/main/java/com/example/chessapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.chessapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // In Stage 2, we will add code here to find all the buttons
+        // and the chessboard and add logic to them.
+        // For now, we just show the layout.
+    }
+}
